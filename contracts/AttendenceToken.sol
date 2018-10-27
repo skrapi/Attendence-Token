2127f1ce3aabf557edf81b5bf08f855f9fdc3eda
Sylvan Morris <sylvan.j.morris@gmail.com>
2018-10-27 20:59:21 +0200

    Changed to an ERC721 token

--- contracts/AttendenceToken.sol
+++ contracts/AttendenceToken.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.4.25;
 
 
-import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol";
 
 
-contract AttendenceToken is Ownable{
+contract AttendenceToken is ERC721Mintable {
     string name;
     string symbol;
     
@@ -14,30 +14,7 @@ contract AttendenceToken is Ownable{
             symbol = _symbol;
         }
     
-    mapping (uint16 => mapping( address => bool)) public attendence;
-    
-    mapping(string => uint16) tokenId;
-    
-    uint16[] token;
-    
-    
-    
-    function createToken(string _tokenName) public onlyOwner() {
-        attendence[_tokenName][msg.sender] = true;
-        tokenId[_tokenName] = token.push(_tokenId)-1;
-        
-    }
-    
-    function mint(address _signee, uint16 _tokenId) public onlyOwner() {
-     // TODO implement minting process
-     
-    }
-    
-    
-    // TODO implement newToken - to verify it hasn't been created before
-    modifier newToken(_tokenName) {
-       
-    }
+
     
     
 }
